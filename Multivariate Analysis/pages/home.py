c2bc9710b5c16b16132cc483a6e3e673dd54c0db
giovanni costa <68923520+giovanniwdc@users.noreply.github.com>
2023-11-25 16:59:11 -0300

    adding: decimal, sep and encoding selectors

--- Multivariate Analysis/pages/home.py
+++ Multivariate Analysis/pages/home.py
@@ -2,15 +2,29 @@ import dash
 from dash import dcc, html, Input, Output, State,  callback, dash_table
 from collections import OrderedDict
 
+import pandas as pd
 
 import json
-from scripts.dataset import Dataset
+from scripts.dataset import Dataset, DataDict
 
 DATA_DIR = './datasets'
 EXTERNAL_STYLESHEETS = ['/assets/home.css']
 
+DELIMITER_OPTIONS={
+    ';':';',
+    ',':',',
+    "' '":""
+}
 
-dataset = Dataset()
+DECIMAL_OPTIONS={
+    ',':',',
+    '.':'.'
+}
+
+ENCODING_OPTIONS={
+    'Latin 1':'latin-1',
+    'UTF-8':'utf-8'
+}
 
 dash.register_page(__name__,external_stylesheets=EXTERNAL_STYLESHEETS, path='/')
 
@@ -19,12 +33,35 @@ with open(DATA_DIR+"/datasets.json",  encoding='utf-8') as file:
 
 layout = html.Div(
     [   
-        dcc.Dropdown(
-            options=[{"label": meta["name"] , "value": meta["file_name"]} for meta in datasetList],
-            searchable=False,
-            placeholder="Select a dataset",
-            id="data-list-dropdown"
-        ),
+        html.Div([
+            dcc.Dropdown(
+                options=[{"label": meta["name"] , "value": meta["file_name"]} for meta in datasetList],
+                searchable=False,
+                placeholder="Select a dataset",
+                id="data-list-dropdown"
+            ),
+            dcc.Dropdown(
+                options=[{"label": option, "value": DELIMITER_OPTIONS[option]} for option in DELIMITER_OPTIONS.keys()],
+                searchable=False,
+                placeholder="Select a delimiter",
+                id="delimiter-list"
+            ),    
+            dcc.Dropdown(
+                options=[{"label": option, "value": DECIMAL_OPTIONS[option]} for option in DECIMAL_OPTIONS.keys()],
+                searchable=False,
+                placeholder="Select a decimal",
+                id="decimal-list"
+            ),    
+            dcc.Dropdown(
+                options=[{"label": option, "value": ENCODING_OPTIONS[option]} for option in ENCODING_OPTIONS.keys()],
+                searchable=False,
+                placeholder="Select a encoding",
+                id="encoding-list"
+            ),    
+        ]),
+        html.Div([
+            
+        ], className="data-options"),
         html.Div([
 
         ], id="dataset-show"),
@@ -36,11 +73,20 @@ layout = html.Div(
 @callback(
     Output("dataset-show", "children"),
     Input("data-list-dropdown", "value"), 
+    Input("delimiter-list", 'value'),
+    Input("decimal-list", 'value'),
+    Input("encoding-list", 'value'),
     prevent_initial_call=True
 )
-def update_dataset(selected_data_path):
+def update_dataset(selected_data_path, sep:str=None, decimal:str=None, encoding:str=None):
 
-    df = Dataset(DATA_DIR+selected_data_path).getData()
+    if decimal == None:
+        decimal = ','
+
+    data_dict = DataDict()
+    data_dict.add_dataset('df', pd.read_csv(DATA_DIR+selected_data_path, sep=sep, decimal=decimal, encoding=encoding))
+    
+    df = data_dict.get_dataset('df')
 
     return html.Div([
         dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], page_size=10)
--- Multivariate Analysis/scripts/dataset.py
+++ Multivariate Analysis/scripts/dataset.py
@@ -2,12 +2,18 @@ import pandas as pd
 
 from scripts.decorators import singleton
 
+READ_CSV_CONFIG={
+    "sep":";",
+    "decimal":","
+}
+
 @singleton
 class Dataset:
     def __init__(self, path=None):
 
         self._path = path
         self._data = None
+        self.csv_config=READ_CSV_CONFIG
 
         if path != None:
             self.setData(path)
@@ -15,9 +21,9 @@ class Dataset:
     def getData(self):
         return self._data
 
-    def setData(self, path):
+    def setData(self, path, **kwargs):
         self._path = path
-        self._data = self._opendDataset(path)
+        self._data = self._opendDataset(path, *kwargs)
 
     def _opendDataset(self, path):
         try:
@@ -25,3 +31,19 @@ class Dataset:
             return data
         except FileNotFoundError as error:
             print("File path not found!")
+
+@singleton
+class DataDict:
+
+    def __init__(self):
+        self._data_dict = {}
+
+    def add_dataset(self, name, dataset):
+        self._data_dict[name] = dataset
+    
+    def get_dataset(self, name):
+        return self._data_dict[name]
+
+    def __str__(self):
+        print(f"Dataset list {self._data_dict.keys()}")
+
